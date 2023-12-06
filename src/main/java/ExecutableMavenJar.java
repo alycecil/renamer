2fb538a93b8706f4c2338afaa4e2e8c270d200b1
Alice <alycecil@gmail.com>
2023-12-05 20:15:07 -0500

    yup

--- src/main/java/ExecutableMavenJar.java
+++ src/main/java/ExecutableMavenJar.java
@@ -2,7 +2,9 @@ import ui.CommonForm;
 
 public class ExecutableMavenJar {
     public static void main(String[] args) {
+        System.out.println("Started");
         CommonForm.main(args);
+        System.out.println("Ended");
     }
 
 }
